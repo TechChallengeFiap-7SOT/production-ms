b99032b6624463c08a2be293c5020bc7a97bbd10
Murakami <maurimurakami97@gmail.com>
2024-12-03 19:45:16 -0300

    add ok route

--- src/main/routes/pedidos_route.py
+++ src/main/routes/pedidos_route.py
@@ -6,6 +6,11 @@ from src.main.adapter import flask_adapter
 pedido_route_bp = Blueprint('pedidos', __name__)
 
 
+@pedido_route_bp.route('/', methods=['GET'])
+def ok():
+    return jsonify({'msg': 'ok'}), 200
+
+
 @pedido_route_bp.route('/register_pedido', methods=['POST'])
 def register_pedido():
     response = flask_adapter(request=request, api_route=register_pedido_composer())
@@ -18,7 +23,7 @@ def register_pedido():
     return jsonify(message), response.status_code
 
 
-@pedido_route_bp.route('/find_pedidos', methods=['GET'])
+@pedido_route_bp.route('/find_pedidos', methods=['POST'])
 def find_pedidos():
     response = flask_adapter(request=request, api_route=find_pedidos_composer())
 
